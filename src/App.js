13889e8ed3cc3cc532562844f4a96c245644cd34
aminedhobb <amine.dhobb@gmail.com>
2020-01-13 19:01:49 +0100

    fetch search result on component update

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import axios from 'axios'
 
 export default function App() {
   const [results, setResults] = useState([])
+  const [query, setQuery] = useState('reacthooks')
 
   useEffect(() => {
     getResults()
-  }, [])
+  }, [query])
 
   const getResults = async () => {
     const response = await axios
-      .get('http://hn.algolia.com/api/v1/search?query=reacthooks')
+      .get(`http://hn.algolia.com/api/v1/search?query=${query}`)
     setResults(response.data.hits)
   }
 
   return(
     <>
+      <input type="text" onChange={event => setQuery(event.target.value)} />
       <ul>
         {results && results.map((result) => (
           <li key={result.objectID}>
