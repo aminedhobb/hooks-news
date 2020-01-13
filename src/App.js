6a5d9f05efb188e62964f98b2eb869945fb07ea8
aminedhobb <amine.dhobb@gmail.com>
2020-01-13 19:32:33 +0100

    handle error

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
   const [results, setResults] = useState([])
   const [query, setQuery] = useState('react hooks')
   const [isLoading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
   const searchInputRef = useRef()
 
   useEffect(() => {
@@ -13,9 +14,13 @@ export default function App() {
 
   const getResults = async () => {
     setLoading(true)
-    const response = await axios
-      .get(`http://hn.algolia.com/api/v1/search?query=${query}`)
-    setResults(response.data.hits)
+    try {
+      const response = await axios
+        .get(`http://hn.algolia.com/api/v1/search?query=${query}`)
+      setResults(response.data.hits)
+    } catch (error) {
+      setError(error)
+    }
     setLoading(false)
   }
 
@@ -53,6 +58,7 @@ export default function App() {
           </ul>
         )
       }
+      {error && <div>{error.message}</div>}
     </>
   )
 }
